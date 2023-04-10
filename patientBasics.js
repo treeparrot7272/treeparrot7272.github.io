0325a395a71b547006ad9bad18699d5a40bf5603
treeparrot7272 <65615431+treeparrot7272@users.noreply.github.com>
2023-04-10 12:14:35 -0500

    Add files via upload

--- patientBasics.js
+++ patientBasics.js
@@ -1,4 +1,6 @@
-import { createTextInput, renderRadioButtons } from './StructureBuilders/renderInputs.js'
+import { createTextInput, renderCheckboxList, renderRadioButtons } from './StructureBuilders/renderInputs.js'
+
+import { cardinals } from './DataSets/cardinals.js';
 
 document.addEventListener('click', updateResults)
 
@@ -16,6 +18,9 @@ let age = document.getElementById('ageTextBox');
 renderRadioButtons(['M', 'F'], 'gender');
 //value gets drawn below, due to not being a static element
 
+//presentations
+renderCheckboxList(cardinals, 'Presentations')
+
 
 function updateResults() {
     
@@ -24,5 +29,21 @@ function updateResults() {
     //get gender; evaluate live because checked nature?
     const gender = document.querySelector("input[name='gender']:checked");
 
-    textPlace.innerText = `${firstName.value} ${lastName.value} is a ${age.value}${gender.value} who presented to hospital with `
+    //cardinal pullout
+    //tabulate what has been scored
+    const presentation = document.querySelectorAll("input[name='Presentations']:checked");
+        
+    //formulate message
+    let presentationMessage = ''
+    //create message with each selection, then add a comma
+    presentation.forEach(cardinalChosen => {
+        presentationMessage += cardinalChosen.value + ', '    })
+        //add a period to the end, allows for regex
+    presentationMessage += '.' 
+    
+
+    //take out last , and put in period
+    //presentationMessage = message.replace(/,\s\./i, '.')
+    
+    textPlace.innerText = `${firstName.value} ${lastName.value} is a ${age.value}${gender.value} who presented with ${presentationMessage}`
 }
