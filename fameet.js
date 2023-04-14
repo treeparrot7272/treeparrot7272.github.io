c60303c6de16681bfd4acf238adbad7d3b47e7c0
treeparrot7272 <65615431+treeparrot7272@users.noreply.github.com>
2023-04-14 11:26:20 -0500

    Add files via upload

--- fameet.js
+++ fameet.js
@@ -1,26 +1,41 @@
-import {createTextInput } from "./StructureBuilders/renderInputs.js"
-let data = ['firstName', 'lastName', 'familyName', 'familyRelation', 'issueOfConcern', 'solutionToConcern']
+import {createTextInput, renderRadioButtons } from "./StructureBuilders/renderInputs.js"
+let data = ['firstName', 'lastName', 'familyName', 'familyRelation', 'minutes']
 
 data.forEach(title => {
     createTextInput(title);
 })
+renderRadioButtons(['Patient Present', 'Patient not Present'], 'presenceOfPatient')
 
-createTextInput('minutes');
 
-document.addEventListener('click', updateResults)
+function createButton(whatSays, whatCalled, whatDoes){
+    let container = document.createElement('section')
+    container.setAttribute('id', `${whatCalled}ButtonContainer`)
+
+    let buttonElement = document.createElement('button');
+    
+    buttonElement.setAttribute('id', `${whatCalled}Button`)
+    buttonElement.innerHTML = whatSays
+    buttonElement.addEventListener('click', whatDoes)
+
+    container.appendChild(buttonElement)
+
+    document.querySelector('main').append(container)
+}
+
+createButton('Update Results', 'ResultButton', updateResults)
+
+
 
 let firstName = document.getElementById('firstNameTextBox');
-let lastName = document.getElementById('lastNameTextBox');
 let familyName = document.getElementById('familyNameTextBox');
 let familyRelation = document.getElementById('familyRelationTextBox');
 let totalTime = document.getElementById('minutesTextBox');
-let issueOfConcern = document.getElementById('issueOfConcernTextBox');
-let solutionToConcern = document.getElementById('solutionToConcernTextBox');
+
 
 
     function updateResults(){ 
-    let discussionVariations = [`I was able to speak to ${firstName.value} and their ${familyRelation.value} ${familyName.value}. I provided an update on their care and plan going forward. In particular, they had questions about ${issueOfConcern.value}. I let them know that  we will be ${solutionToConcern.value}.
-        Total time of our conversation was ${totalTime.value} minutes.`, `During our ${totalTime.value}-minute conversation, I had the opportunity to update ${firstName.value} and their ${familyRelation.value} ${familyName.value} on their care and the plan for moving forward. They had some concerns about ${issueOfConcern.value}, but I reassured them that we plan to ${solutionToConcern.value}.`, `In a ${totalTime.value}-minute conversation, I updated both ${firstName.value} and their ${familyRelation.value} ${familyName.value} regarding their care and the plan for the future. They were specifically concerned about ${issueOfConcern.value} and I let them know we plan to ${solutionToConcern.value}.`, `In a ${totalTime.value}-minute conversation, I provided an update on the care and plan for ${firstName.value} and their ${familyRelation.value} ${familyName.value}. They had some questions and concerns about ${issueOfConcern.value}, but I was able to reassure them by outlining the solution we have in place to deal with this.`]
+    let discussionVariations = [`I was able to speak to ${firstName.value} and their ${familyRelation.value} ${familyName.value}. I provided an update on their care and plan going forward. 
+        Total time of our conversation was ${totalTime.value} minutes.`, `During our ${totalTime.value}-minute conversation, I had the opportunity to update ${firstName.value} and their ${familyRelation.value} ${familyName.value} on their care and the plan for moving forward.`, `In a ${totalTime.value}-minute conversation, I updated both ${firstName.value} and their ${familyRelation.value} ${familyName.value} regarding their care and the plan for the future.`, `In a ${totalTime.value}-minute conversation, I provided an update on the care and plan for ${firstName.value} and their ${familyRelation.value} ${familyName.value}.`]
 
     let textPlace = document.getElementById('textArea')
     let randomNumber = Math.floor(Math.random()*4)
