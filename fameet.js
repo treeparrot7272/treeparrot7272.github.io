57531f0a0ec9e4772bbbf1d6c650850eceda5bf2
treeparrot7272 <65615431+treeparrot7272@users.noreply.github.com>
2023-04-15 13:45:17 -0500

    Add files via upload

--- fameet.js
+++ fameet.js
@@ -33,11 +33,33 @@ let totalTime = document.getElementById('minutesTextBox');
 
 
 
-    function updateResults(){ 
-    let discussionVariations = [`I was able to speak to ${firstName.value} and their ${familyRelation.value} ${familyName.value}. I provided an update on their care and plan going forward. 
-        Total time of our conversation was ${totalTime.value} minutes.`, `During our ${totalTime.value}-minute conversation, I had the opportunity to update ${firstName.value} and their ${familyRelation.value} ${familyName.value} on their care and the plan for moving forward.`, `In a ${totalTime.value}-minute conversation, I updated both ${firstName.value} and their ${familyRelation.value} ${familyName.value} regarding their care and the plan for the future.`, `In a ${totalTime.value}-minute conversation, I provided an update on the care and plan for ${firstName.value} and their ${familyRelation.value} ${familyName.value}.`]
+function updateResults(){ 
+    
+    
+    let presentOrNot = document.querySelector('input[name="presenceOfPatient"]:checked').value;
+    
+    
+    let discussionVariations = {
+        'Patient Present': 
+    [`I was able to speak to ${firstName.value} and their ${familyRelation.value} ${familyName.value}. I provided an update on their care and plan going forward. Total time of our conversation was ${totalTime.value} minutes.`, 
+        `During our ${totalTime.value}-minute conversation, I had the opportunity to update ${firstName.value} and their ${familyRelation.value} ${familyName.value} on their care and the plan for moving forward.`, 
+        `In a ${totalTime.value}-minute conversation, I updated both ${firstName.value} and their ${familyRelation.value} ${familyName.value} regarding their care and the plan for the future.`, 
+        `In a ${totalTime.value}-minute conversation, I provided an update on the care and plan for ${firstName.value} and their ${familyRelation.value} ${familyName.value}.`],
+    'Patient not Present': 
+        [`I was able to speak to ${firstName.value}'s ${familyRelation.value} ${familyName.value}. I provided an update on ${familyName.value} care and plan going forward. Total time of our conversation was ${totalTime.value} minutes.`, 
+        `During a ${totalTime.value}-minute conversation, I had the opportunity to update ${firstName.value}'s  ${familyRelation.value} ${familyName.value} on the care and the plan for moving forward.`, 
+        `In a ${totalTime.value}-minute conversation, I updated ${firstName.value}'s ${familyRelation.value} ${familyName.value} regarding their care and the plan for the future.`, 
+        `I was able to speak to patient's family today. In a ${totalTime.value}-minute conversation, I provided an update on the care and plan for ${firstName.value}'s  ${familyRelation.value} ${familyName.value}.`]
+    }
 
     let textPlace = document.getElementById('textArea')
     let randomNumber = Math.floor(Math.random()*4)
-    textPlace.innerText = discussionVariations[randomNumber]
-    };
+    
+    if (presentOrNot !== null) { textPlace.innerText = discussionVariations[presentOrNot][randomNumber]
+    } else {textPlace.innerText = "Indicate whether or not the patient was present."
+    }
+    
+    
+    
+};
+
